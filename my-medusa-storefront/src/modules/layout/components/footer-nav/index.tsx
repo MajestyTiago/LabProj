f987ddf50c48e57501cf556acee9779320376860
PAULO RAFAEL RODRIGUES COELHO <8210132@estg.ipp.pt>
2022-11-25 15:13:58 +0000

    Footer Layout

--- my-medusa-storefront/src/modules/layout/components/footer-nav/index.tsx
+++ my-medusa-storefront/src/modules/layout/components/footer-nav/index.tsx
@@ -9,12 +9,10 @@ const FooterNav = () => {
   return (
     <div className="content-container flex flex-col gap-y-8 pt-16 pb-8">
       <div className="flex flex-col gap-y-6 xsmall:flex-row items-start justify-between">
-        <div>
-          <Link href="/">
-            <a className="text-xl-semi uppercase">LP</a>
-          </Link>
-        </div>
-        <div className="text-small-regular grid grid-cols-2 gap-x-16">
+        <div className="text-small-regular grid grid-cols-6  gap-x-16">
+          {/* support */}
+          <div className="flex flex-col gap-y-2"></div>
+          {/* collections */}
           <div className="flex flex-col gap-y-2">
             <span className="text-base-semi">Collections</span>
             <ul
@@ -31,46 +29,99 @@ const FooterNav = () => {
               ))}
             </ul>
           </div>
+          {/* gap1 */}
           <div className="flex flex-col gap-y-2">
-            <span className="text-base-semi">Medusa</span>
+            <span className="text-base-semi">Support</span>
             <ul className="grid grid-cols-1 gap-y-2">
               <li>
-                <a
-                  href="https://github.com/medusajs"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  GitHub
-                </a>
+                <Link href={""}>
+                  <a className="underlineForLinks">Get Help</a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="https://docs.medusajs.com"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Documentation
-                </a>
+                <Link href={""}>
+                  <a className="underlineForLinks">Registration & Warranty</a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="https://github.com/medusajs/nextjs-starter-medusa"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Source code
-                </a>
+                <Link href={""}>
+                  <a className="underlineForLinks">LP Store Support</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          {/* gap2 */}
+          <div className="flex flex-col gap-y-2">
+            <span className="text-base-semi">Support</span>
+            <ul className="grid grid-cols-1 gap-y-2">
+              <li>
+                <Link href={""}>
+                  <a className="underlineForLinks">Get Help</a>
+                </Link>
+              </li>
+              <li>
+                <Link href={""}>
+                  <a className="underlineForLinks">Registration & Warranty</a>
+                </Link>
+              </li>
+              <li>
+                <Link href={""}>
+                  <a className="underlineForLinks">LP Store Support</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          {/* Company */}
+          <div className="flex flex-col gap-y-2">
+            <span className="text-base-semi">Company</span>
+            <ul className="grid grid-cols-1 gap-y-2">
+              <li>
+                <Link href={""}>
+                  <a className="underlineForLinks">About Us</a>
+                </Link>
+              </li>
+              <li>
+                <Link href={""}>
+                  <a className="underlineForLinks">Careers</a>
+                </Link>
+              </li>
+              <li>
+                <Link href={""}>
+                  <a className="underlineForLinks">Contact Us</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          {/* social media */}
+          <div className="flex flex-col gap-y-2">
+            <span className="text-base-semi">Social Media</span>
+            <ul className="grid grid-cols-1 gap-y-2">
+              <li>
+                <Link href={""}>
+                  <a className="underlineForLinks">Facebook</a>
+                </Link>
+              </li>
+              <li>
+                <Link href={""}>
+                  <a className="underlineForLinks">Instagram</a>
+                </Link>
+              </li>
+              <li>
+                <Link href={""}>
+                  <a className="underlineForLinks">Twitter</a>
+                </Link>
               </li>
             </ul>
           </div>
         </div>
       </div>
+
       <div className="flex flex-col-reverse gap-y-4 justify-center xsmall:items-center xsmall:flex-row xsmall:items-end xsmall:justify-between">
         <span className="text-xsmall-regular text-gray-500">
           © Copyright 2022 LP
         </span>
         <div className="min-w-[316px] flex xsmall:justify-end">
-          <CountrySelect />
+          {/* <CountrySelect />  Cena do Shipping To */}
         </div>
       </div>
     </div>
